2b0dcca154634c825385476e2bce848a36b10aff
Vitja72 <viktor.stefan@tn.techstarter.de>
2025-08-04 10:37:33 +0200

    RC-001: Implemented the basic app server

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+const port = 5002;
+app.get('/', (req,res) => {
+    res.send('Welcome to Ressource Catalog');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
