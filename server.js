1e82c7d933f4dffa49d349d62aa97a05a4cf7311
Vitja72 <viktor.stefan@tn.techstarter.de>
2025-08-05 12:47:37 +0200

    RC-005: Implemented the post /resources endpoint.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import resourcesRouter from './routers/resources.js';
-
-
-const app = express();
+import resourcesRouter from './routes/resources.js';
 
 const port = 5002;
 
+const app = express();
+
+// Middleware
 app.use(express.json());
+
+// Routes
 app.use('/resources', resourcesRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
